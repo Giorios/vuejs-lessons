ed968cfe50a290955828484f51fd47111e505d5c
Giorios <90624324+Giorios@users.noreply.github.com>
2024-02-22 08:13:14 -0500

    lesson 4 complete

--- Intro-to-Vue-3-master/main.js
+++ Intro-to-Vue-3-master/main.js
@@ -5,6 +5,8 @@ const app = Vue.createApp ({
             description: 'Cool socks',
             image: './assets/images/socks_green.jpg',
             url: 'https://www.vuemastery.com/',
+            inventory: 100,
+            onSale: true,
         }
     }
 })
