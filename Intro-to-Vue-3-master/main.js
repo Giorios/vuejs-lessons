c702a2024480c0cd85d4d15fd2e6b673262b82d4
Giorios <90624324+Giorios@users.noreply.github.com>
2024-02-22 08:03:28 -0500

    lesson 3 complete

--- Intro-to-Vue-3-master/main.js
+++ Intro-to-Vue-3-master/main.js
@@ -2,7 +2,9 @@ const app = Vue.createApp ({
     data: function() {
         return {
             product: 'Socks',
-            description: 'Cool socks'
+            description: 'Cool socks',
+            image: './assets/images/socks_green.jpg',
+            url: 'https://www.vuemastery.com/',
         }
     }
 })
