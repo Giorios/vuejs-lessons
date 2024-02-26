420f968f3447b970ebc339fe097ad6fa6e636223
Giorios <90624324+Giorios@users.noreply.github.com>
2024-02-26 10:59:00 -0500

    lesson 7 partially complete

--- main.js
+++ main.js
@@ -6,7 +6,8 @@ const app = Vue.createApp ({
             description: 'Cool socks',
             image: './assets/images/socks_green.jpg',
             url: 'https://www.vuemastery.com/',
-            inventory: 100,
+            inStock: true,
+            // inventory: 100,
             onSale: true,
             details:['50% cotton', '30% wool', '20% polyester'],
             variants: [
@@ -26,6 +27,6 @@ const app = Vue.createApp ({
         },
         updateImage(variantImage) {
             this.image = variantImage;
-        }
+        },       
     }
 })
